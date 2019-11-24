747b5387066f1a7bcff17ad873c267587300d168
staciajohanna <edinastacia@gmail.com>
2019-11-24 17:13:03 -0500

    include skeleton of Boid class

---
+++ src/boid.cpp
@@ -0,0 +1,18 @@
+#include "boid.h"
+
+#include "camera.h"
+#include "vertexrecorder.h"
+
+Boid::Boid()
+{
+    
+}
+
+std::vector<Vector3f> Boid::evalF(std::vector<Vector3f> state)
+{
+}
+
+// render the system (ie draw the particles)
+void Boid::draw(GLProgram& gl)
+{
+}
