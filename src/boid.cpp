fa6ba89bcf83c00b31a19f01d0f03845941ace60
Charvi Gopal <charvig@mit.edu>
2019-12-03 19:22:19 -0500

    Seeking

--- src/boid.cpp
+++ src/boid.cpp
@@ -1,4 +1,11 @@
 #include "boid.h"
+#include "gl.h"
+#include <GLFW/glfw3.h>
+
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 
 #include "camera.h"
 #include "vertexrecorder.h"
@@ -8,8 +15,9 @@ const float NEIGHBOR_RADIUS = 5.0;
 const float WEIGHT_SEPARATION = 1.5;
 const float WEIGHT_ALIGNMENT = 1.0;
 const float WEIGHT_COHESION = 1.0;
-const float WEIGHT_SEEK = 1.0;
+const float WEIGHT_SEEK = 4.0;
 const float WEIGHT_COLLISION_AVOIDANCE = 1.0;
+Vector3f cursorPosition;
 
 Boid::Boid()
 {
@@ -75,7 +83,26 @@ Vector3f getAlignmentForce(std::vector<Vector3f> &state, int birdIndex)
 
 Vector3f getSeekForce(std::vector<Vector3f> &state, int birdIndex) 
 {
-
+        
+        Vector3f &curPos = state[birdIndex * 2];
+        Vector3f &curVel = state[birdIndex * 2 + 1];
+        // double xd, yd;
+        // glfwGetCursorPos(window, &xd, &yd);
+        // cursorX = (int)xd;
+        // cursorY = (int)yd;
+        float targetX = (cursorPosition[0] - 512.0f) * (4.0f/1024.0f);
+        float targetY = -1 * (cursorPosition[1] - 402.0f) * (4.0f/804.0f);
+        // float targetX = -10.0 - (1024 / (cursorPosition[0] + 1) * 2);
+        // float targetY = 10 - (804 / (cursorPosition[1] + 1) * 2);
+        // Vector3f target = Vector3f(targetX, targetY, 0);
+        Vector3f target = Vector3f(targetX, targetY, 0);
+        std::cout << cursorPosition[0] << "cursorX IS PRINTING \n";
+        std::cout << cursorPosition[1] << "cursorY IS PRINTING \n";
+        std::cout << targetX << "targetX IS PRINTING \n";
+        std::cout << targetY << "targetY IS PRINTING \n";
+        Vector3f desiredVelocity = (target - curPos).normalized();
+        Vector3f steeringForce = desiredVelocity - curVel;
+        return steeringForce;
 }
 
 Vector3f getCollisionAvoidanceForce(std::vector<Vector3f> &state, int birdIndex) 
@@ -92,7 +119,7 @@ std::vector<Vector3f> Boid::evalF(std::vector<Vector3f> state)
         Vector3f separationForce = getSeparationForce(state, i);
         Vector3f alignmentForce = getAlignmentForce(state, i);
         Vector3f cohesionForce = vel + alignmentForce;
-        Vector3f seekForce = Vector3f(0,0,0);
+        Vector3f seekForce = getSeekForce(state, i);
         Vector3f collisionAvoidanceForce = Vector3f(0,0,0);
         Vector3f netForce = WEIGHT_SEPARATION * separationForce + 
                             WEIGHT_ALIGNMENT * alignmentForce + 
@@ -115,9 +142,13 @@ void Boid::draw(GLProgram& gl)
     for (int i = 0; i < currentState.size()/2; ++i) {
         Vector3f position = currentState[i * 2];
         Vector3f velocity = currentState[i * 2 + 1];
-        Vector3f a = Vector3f(0, 1, 0);
+        Vector3f a = Vector3f(1, 1, 0);
         float rad = acos((Vector3f::dot(a, velocity))/(a.abs() * velocity.abs()));
-        gl.updateModelMatrix(Matrix4f::translation(position)*Matrix4f::rotation(Vector3f(0, 0, -1), rad));
+        gl.updateModelMatrix(Matrix4f::translation(position)*Matrix4f::rotation(Vector3f(0,0, -1), rad));
         drawBird(0.15f);
     } 
+}
+
+void Boid::setCursorPosition(int cursorX, int cursorY) {
+    cursorPosition = Vector3f(cursorX, cursorY, 0);
 }
